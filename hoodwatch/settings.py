76858c2869a643c0e25654667b3571212c08f787
dhalianeza <dhalianeza02@gmail.com>
2021-02-02 12:01:10 +0200

    save changes

--- hoodwatch/settings.py
+++ hoodwatch/settings.py
@@ -99,7 +99,7 @@ else:
    DATABASES = {
        'default': dj_database_url.config(
            default=config('DATABASE_URL')
-       )
+        )
     }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
